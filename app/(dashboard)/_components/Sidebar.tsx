d3d530d3efeea7e9e74f84a3d4b4f495d9c5eb52
Simen Daehlin <simen.daehlin@strapi.io>
2023-09-18 16:36:08 +0100

    Completed sidebar missing mobile

--- app/(dashboard)/_components/Sidebar.tsx
+++ app/(dashboard)/_components/Sidebar.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Logo } from './Logo';
+import { SidebarRoutes } from './SidebarRoutes';
 
 interface SidebarProps { }
 
-export const Sidebar: React.FC<SidebarProps> = async () => {
+export const Sidebar: React.FC<SidebarProps> = () => {
   return (
     <div className='h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm'>
-      Sidebar
+      <div className='p-6'>
+        <Logo />
+      </div>
+      <div className='flex flex-col w-full'>
+        <SidebarRoutes />
+      </div>
     </div>
   );
 }
