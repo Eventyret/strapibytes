4c9c8a441a1da1751b412d5875b0a439d8792553
Simen Daehlin <simen.daehlin@strapi.io>
2023-09-19 13:59:42 +0100

    Started working on SingleCourse

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,3 +1,6 @@
+import { getUser } from '@/lib/auth/auth';
+import { db } from '@/lib/prisma';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
 interface SingleCourseProps {
@@ -7,9 +10,40 @@ interface SingleCourseProps {
 }
 
 const SingleCourse: React.FC<SingleCourseProps> = async ({ params }) => {
+  const user = await getUser()
+  if (!user) {
+    return redirect('/')
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId
+    }
+  })
+  if (!course) {
+    return redirect('/')
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId
+  ]
+  const totalFields = requiredFields.length
+  const completedFields = requiredFields.filter(Boolean).length
+  const completionText = `(${completedFields}/${totalFields})`
   return (
-    <div>
-      { params.courseId }
+    <div className='p-6'>
+      <div className='flex items-center justify-between'>
+        <div className='flex flex-col gap-y-2'>
+          <h1 className='text-2xl font-medium'>
+            Course Setup
+          </h1>
+          <span>Complete all fields { completionText }</span>
+        </div>
+      </div>
     </div>
   );
 }
