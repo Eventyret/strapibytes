5900e962d8b8bed6ab8b7e316b9fdab19c27604b
Simen Daehlin <simen.daehlin@strapi.io>
2023-09-19 14:05:26 +0100

    Added iconbadge

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,7 @@
+import { IconBadge } from '@/components/icon-badge';
 import { getUser } from '@/lib/auth/auth';
 import { db } from '@/lib/prisma';
+import { LayoutDashboard } from 'lucide-react';
 import { redirect } from 'next/navigation';
 import React from 'react';
 
@@ -36,12 +38,21 @@ const SingleCourse: React.FC<SingleCourseProps> = async ({ params }) => {
   const completionText = `(${completedFields}/${totalFields})`
   return (
     <div className='p-6'>
+
       <div className='flex items-center justify-between'>
         <div className='flex flex-col gap-y-2'>
           <h1 className='text-2xl font-medium'>
             Course Setup
           </h1>
           <span>Complete all fields { completionText }</span>
+        </div>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-16'>
+        <div>
+          <div className='flex items-center gap-x-2'>
+            <IconBadge icon={ LayoutDashboard } />
+            <h2 className='text-xl'>Customize your course</h2>
+          </div>
         </div>
       </div>
     </div>
