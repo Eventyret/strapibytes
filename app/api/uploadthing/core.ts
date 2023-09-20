e7f523aa0722b92fc762bf7139dcd2a4ab489940
Simen Daehlin <simen.daehlin@strapi.io>
2023-09-20 11:50:56 +0100

    Created route for uploadthing

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,25 @@
+import { authOptions } from '@/lib/auth/next-auth-config';
+import { getServerSession } from 'next-auth';
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+const handleAuth = async () => {
+  const session = await getServerSession(authOptions);
+  if (!session) throw new Error("Unauthorized");
+
+  return { userId: session.user?.id };
+};
+
+export const ourFileRouter = {
+  courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(async () => await handleAuth())
+    .onUploadComplete(async () => { }),
+  courseAttachment: f(["text", "image", "video", "audio", "pdf"])
+    .middleware(async () => await handleAuth())
+    .onUploadComplete(async () => { }),
+  chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: "512GB" } })
+    .middleware(async () => await handleAuth())
+    .onUploadComplete(async () => { })
+} satisfies FileRouter;
+export type OurFileRouter = typeof ourFileRouter;
