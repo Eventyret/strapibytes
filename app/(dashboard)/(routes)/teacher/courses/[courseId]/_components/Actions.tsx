24049381e754b497c08162f174e4d15bd07855f3
Simen Daehlin <simen.daehlin@strapi.io>
2023-11-06 14:01:43 +0000

    Created action for course

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/Actions.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+import { Button } from "@/components/ui/button";
+
+interface ActionsProps {
+  disabled: boolean;
+  courseId: string;
+  isPublished: boolean;
+}
+
+export const Actions = ({ disabled, courseId, isPublished }: ActionsProps) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    setIsLoading(true);
+    const action = isPublished ? "unpublish" : "publish";
+    const successMessage = isPublished
+      ? "Course offline. It's intermission time!"
+      : "Showtime! Course is now live.";
+
+    await toast.promise(axios.patch(`/api/courses/${courseId}/${action}`), {
+      loading: "Setting the stage...",
+      success: () => successMessage,
+      error: "Oops! The stage lights tripped.",
+    });
+
+    router.refresh();
+    setIsLoading(false);
+  };
+
+  const onDelete = async () => {
+    setIsLoading(true);
+    await toast.promise(axios.delete(`/api/courses/${courseId}`), {
+      loading: "Dropping the curtain...",
+      success: () => {
+        toast.success("Course has ended its run.");
+        router.refresh();
+        router.push(`/teacher/courses`);
+        return "Final bow taken!";
+      },
+      error: () => "Encore? That wasn't planned.",
+    });
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="flex items-center gap-x-2">
+      <Button
+        onClick={onClick}
+        disabled={disabled || isLoading}
+        variant="outline"
+        size="sm">
+        {isPublished ? "Unpublish" : "Publish"}
+      </Button>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button
+          size="sm"
+          disabled={isLoading}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
+};
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,52 +1,59 @@
-import { IconBadge } from '@/components/icon-badge';
-import { getUser } from '@/lib/auth/auth';
-import { db } from '@/lib/prisma';
-import { CircleDollarSign, File, LayoutDashboard, ListChecks } from 'lucide-react';
-import { redirect } from 'next/navigation';
-import React from 'react';
-import { AttachmentForm } from './_components/AttachmentForm';
-import { CategoryForm } from './_components/CategoryForm';
-import { ChaptersForm } from './_components/ChaptersForm';
-import { DescriptionForm } from './_components/DescriptionForm';
-import { ImageForm } from './_components/ImageForm';
-import { PriceForm } from './_components/PriceForm';
-import { TitleForm } from './_components/TitleForm';
+import { Banner } from "@/components/Banner";
+import { IconBadge } from "@/components/icon-badge";
+import { getUser } from "@/lib/auth/auth";
+import { db } from "@/lib/prisma";
+import {
+  CircleDollarSign,
+  File,
+  LayoutDashboard,
+  ListChecks,
+} from "lucide-react";
+import { redirect } from "next/navigation";
+import React from "react";
+import { Actions } from "./_components/Actions";
+import { AttachmentForm } from "./_components/AttachmentForm";
+import { CategoryForm } from "./_components/CategoryForm";
+import { ChaptersForm } from "./_components/ChaptersForm";
+import { DescriptionForm } from "./_components/DescriptionForm";
+import { ImageForm } from "./_components/ImageForm";
+import { PriceForm } from "./_components/PriceForm";
+import { TitleForm } from "./_components/TitleForm";
 
 interface SingleCourseProps {
   params: {
     courseId: string;
-  }
+  };
 }
 
 const SingleCourse: React.FC<SingleCourseProps> = async ({ params }) => {
-  const user = await getUser()
+  const user = await getUser();
   if (!user) {
-    return redirect('/')
+    return redirect("/");
   }
 
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
-      userId: user.id
+      userId: user.id,
     },
     include: {
       chapters: {
         orderBy: {
-          position: 'asc'
-        }
+          position: "asc",
+        },
       },
       attachments: {
         orderBy: {
-          createdAt: 'desc'
-        }
-      }
-    }
-  })
+          createdAt: "desc",
+        },
+      },
+    },
+  });
   if (!course) {
-    return redirect('/')
+    return redirect("/");
   }
 
-  const categories = await db.category.findMany()
+  const categories = await db.category.findMany();
 
   const requiredFields = [
     course.title,
@@ -54,58 +61,93 @@ const SingleCourse: React.FC<SingleCourseProps> = async ({ params }) => {
     course.imageUrl,
     course.price,
     course.categoryId,
-    course.chapters.some(chapter => chapter.isPublished)
-  ]
-  const totalFields = requiredFields.length
-  const completedFields = requiredFields.filter(Boolean).length
-  const completionText = `(${completedFields}/${totalFields})`
-  return (
-    <div className='p-6'>
+    course.chapters.some((chapter) => chapter.isPublished),
+  ];
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+  const completionText = `(${completedFields}/${totalFields})`;
+  const isCompleted = requiredFields.every(Boolean);
 
-      <div className='flex items-center justify-between'>
-        <div className='flex flex-col gap-y-2'>
-          <h1 className='text-2xl font-medium'>
-            Course Setup
-          </h1>
-          <span>Complete all fields { completionText }</span>
-        </div>
-      </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-16'>
-        <div>
-          <div className='flex items-center gap-x-2'>
-            <IconBadge icon={ LayoutDashboard } />
-            <h2 className='text-xl'>Customize your course</h2>
+  return (
+    <>
+      {!course.isPublished && (
+        <Banner label="This course is unpublished - It will not be visible to the students" />
+      )}
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-y-2">
+            <h1 className="text-2xl font-medium">Course Setup</h1>
+            <span>Complete all fields {completionText}</span>
           </div>
-          <TitleForm initialData={ course } courseId={ course.id } />
-          <DescriptionForm initialData={ course } courseId={ course.id } />
-          <ImageForm initialData={ course } courseId={ course.id } />
-          <CategoryForm initialData={ course } courseId={ course.id } options={ categories.map(category => ({ label: category.name, value: category.id })) } />
+          {/* Add Actions */}
+          <Actions
+            disabled={!isCompleted}
+            courseId={params.courseId}
+            isPublished={course.isPublished}
+          />
         </div>
-        <div className='space-y-6'>
-          <>
-            <div className='flex items-center gap-x-2'>
-              <IconBadge icon={ ListChecks } />
-              <h2 className='text-xl'>Course Chapters</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={LayoutDashboard} />
+              <h2 className="text-xl">Customize your course</h2>
             </div>
-            <div>
-              <ChaptersForm initialData={ course } courseId={ course.id } />
-            </div>
+            <TitleForm
+              initialData={course}
+              courseId={course.id}
+            />
+            <DescriptionForm
+              initialData={course}
+              courseId={course.id}
+            />
+            <ImageForm
+              initialData={course}
+              courseId={course.id}
+            />
+            <CategoryForm
+              initialData={course}
+              courseId={course.id}
+              options={categories.map((category) => ({
+                label: category.name,
+                value: category.id,
+              }))}
+            />
+          </div>
+          <div className="space-y-6">
             <>
-              <div className='flex items-center gap-x-2'>
-                <IconBadge icon={ CircleDollarSign } />
-                <h2 className='text-xl'>Sell your course</h2>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={ListChecks} />
+                <h2 className="text-xl">Course Chapters</h2>
               </div>
-              <PriceForm initialData={ course } courseId={ course.id } />
+              <div>
+                <ChaptersForm
+                  initialData={course}
+                  courseId={course.id}
+                />
+              </div>
+              <>
+                <div className="flex items-center gap-x-2">
+                  <IconBadge icon={CircleDollarSign} />
+                  <h2 className="text-xl">Sell your course</h2>
+                </div>
+                <PriceForm
+                  initialData={course}
+                  courseId={course.id}
+                />
+              </>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={File} />
+                <h2 className="text-xl">Resources & Attachments</h2>
+              </div>
+              <AttachmentForm
+                initialData={course}
+                courseId={course.id}
+              />
             </>
-            <div className='flex items-center gap-x-2'>
-              <IconBadge icon={ File } />
-              <h2 className='text-xl'>Resources & Attachments</h2>
-            </div>
-            <AttachmentForm initialData={ course } courseId={ course.id } />
-          </>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
-}
+};
 export default SingleCourse;
